4e23a096cc10e96c13a45b59300414d97da641fc
Malleswari2002-Nakka <malleswarinakka2345@gmail.com>
2025-06-01 01:44:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <App />
+    <Routes>
+      <App />
+    </Routes>
   </HashRouter>
 );
