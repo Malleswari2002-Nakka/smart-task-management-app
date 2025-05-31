a106f3c0c215ba2a1ea93b9ead7a171a00d92bb4
Malleswari2002-Nakka <malleswarinakka2345@gmail.com>
2025-06-01 03:10:36 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './smart-task-management-app/',
+  base: '/smart-task-management-app/',
   plugins: [react()],
 })
