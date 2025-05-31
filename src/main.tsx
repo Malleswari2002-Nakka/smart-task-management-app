2b8c92782c3df9bc47c05eec2dcc4adaa270ccb5
Malleswari2002-Nakka <malleswarinakka2345@gmail.com>
2025-06-01 01:36:51 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
